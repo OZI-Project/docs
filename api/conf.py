b85a4b4e60017a32b49e9c070076bdac94a9ed1c
rjdbcm <rjdbcm@mail.umkc.edu>
2024-02-14 20:38:21 -0600

    remove fontspec package

--- api/conf.py
+++ api/conf.py
@@ -87,7 +87,6 @@ def setup(app: sphinx.application.Sphinx) -> None:
     """Sphinx setup function"""
     app.connect('builder-inited', lambda *_: _Path('TARGET').mkdir(exist_ok=True))
     app.connect('build-finished', lambda *_: rmtree('TARGET'))
-    app.add_latex_package('fontspec')
     app.add_latex_package('atkinson', 'sfdefault')
     app.add_latex_package('notomath')
     app.add_latex_package('lmodern')
