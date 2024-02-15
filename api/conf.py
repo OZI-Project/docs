3d8ba12e8902dfed01da43f4058a8b54399db3c4
rjdbcm <rjdbcm@mail.umkc.edu>
2024-02-14 20:36:35 -0600

    Use pdflatex

--- api/conf.py
+++ api/conf.py
@@ -81,7 +81,6 @@ html_context = {
 }
 
 # -- Options for LaTeX output ------------------------------------------------
-latex_engine = "xelatex"
 latex_show_urls = 'footnote'
 
 def setup(app: sphinx.application.Sphinx) -> None:
